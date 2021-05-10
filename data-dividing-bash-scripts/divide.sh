06a9e033d91540f877c9e581f0c4fe0d0d284397
Ralf Tribrunner <>
2021-05-10 10:42:15 +0200

    bash scripts comment

--- data-dividing-bash-scripts/divide.sh
+++ data-dividing-bash-scripts/divide.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+# A meglévő adathalmazt csoportosítja külön mappába az osztálya (raklap/üres) szerint.
 cd dataset
 one=0
 zero=0
 for filename in *; do
-    if [[ $filename =~ _1.jpg ]]; then 
+    if [[ $filename =~ _1.jpg ]]; then #Ha _1.jpg-t tartalmaz a fájlneve.
 	((one=one+1))	
-	cp $filename ../train/pallet/
+	cp $filename ../train/pallet/ #Másolás a raklapos mappába
 
-    elif [[ $filename =~ _0.jpg ]]; then 
+    elif [[ $filename =~ _0.jpg ]]; then #Ha _0.jpg-t tartalmaz a fájlneve.
 	((zero=zero+1))
-	cp $filename ../train/empty/
+	cp $filename ../train/empty/ #Másolás az üres mappába
     fi
 done
 
--- data-dividing-bash-scripts/validation.sh
+++ data-dividing-bash-scripts/validation.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+#Az adott osztály mappájából áthelyez minden x és 10 közötti elemet.
 
-
-threshold=$1
+threshold=$1 # A threshold (x) paraméterként megadható.
 cd train/pallet
 
 i=1
 for filename in *; do
 	if (( $i >= $threshold )); then 
-	    mv $filename ../../val/pallet
+	    mv $filename ../../val/pallet #Áthelyezés a raklap validációs mappájába.
 	fi
 	((i=i+1))
 	if (( i == 11 )); then 
@@ -21,7 +21,7 @@ cd ../empty
 i=1
 for filename in *; do
 	if (( $i >= $threshold )); then 
-	    mv $filename ../../val/empty
+	    mv $filename ../../val/empty #Áthelyezés a raklap nélküli képek validációs mappájába.
 	fi
 	((i=i+1))
 	if (( i == 11 )); then 
