453d59aad59a56a0b3cc10462a5656761014ec1e
Ralf Tribrunner <>
2021-05-10 10:56:33 +0200

    nodejs szerver comments

--- nodejs-szerver/app.js
+++ nodejs-szerver/app.js
@@ -1,3 +1,4 @@
+// Létrehoz egy szervert, mely a localhost 8080-as portján egy Hello World-el válaszol
 'use strict';
 
 const express = require('express');
@@ -12,5 +13,8 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(PORT, HOST);
+app.listen(PORT, HOST); // szerver elindítása
 console.log(`Running on http://${HOST}:${PORT}`);
+
+// forrás:
+// https://www.digitalocean.com/community/tutorials/nodejs-express-basics
